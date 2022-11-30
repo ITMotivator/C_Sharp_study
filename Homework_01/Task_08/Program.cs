9fec423f6935652be3101e4c5705d0f0befdcc0d
Vyacheslav <slava-russia@mail.ru>
2022-11-30 19:19:06 +0700

    Homework Task_08 completed

---
+++ Homework_01/Task_08/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе 
+// показывает все чётные числа от 1 до N.
+Console.WriteLine("Enter any positive number");
+string numStr = Console.ReadLine();
+int num = Convert.ToInt32(numStr);
+
+for (int i = 1; i <= num; i++)
+{
+if (i % 2 == 0 ) 
+{
+   Console.Write($"{i} "); 
+}
+}
+
+
