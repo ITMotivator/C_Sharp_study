83401171a5a706ad77b14cb2527f8af474b4a74e
Vyacheslav <slava-russia@mail.ru>
2022-12-03 16:15:25 +0700

    Task_15 completed

---
+++ Homework_02/Task_15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+int CreateDay(int a, int b) 
+{
+    Random num = new Random();
+    int res = num.Next(a,b);
+    return res;
+}
+
+bool IsWeekEnd (int day)
+{ 
+    bool res = false;
+    if (day == 6 || day == 7) 
+    {
+        res = true;
+    }
+    return res;
+}
+
+int day = CreateDay(1, 8);
+Console.WriteLine(day);
+Console.WriteLine(IsWeekEnd(day));
