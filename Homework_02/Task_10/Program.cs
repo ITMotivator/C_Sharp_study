32c1e5948014ee066c596a1dc96f722006066ebe
Vyacheslav <slava-russia@mail.ru>
2022-12-03 15:38:11 +0700

    Task_10 completed

---
+++ Homework_02/Task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе 
+// показывает вторую цифру этого числа.
+int CreateNum(int a, int b) 
+{
+    Random num = new Random();
+    int res = num.Next(a,b);
+    return res;
+}
+
+int Show2ndDigit (int num)
+{
+    int res = (num / 10) % 10;
+    return res;
+}
+
+int number = CreateNum(100, 1000);
+Console.WriteLine(number);
+Console.WriteLine(Show2ndDigit(number));
