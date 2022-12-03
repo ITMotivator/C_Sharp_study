f32b21a3e14781ce569d01a7c5628dc0fe728f18
Vyacheslav <slava-russia@mail.ru>
2022-12-03 16:05:43 +0700

    Task_13 completed

---
+++ Homework_02/Task_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+
+Console.Write("Enter any number ");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+if (number > 99)
+{
+    Console.WriteLine(numberStr[2]);
+}
+else 
+{
+   Console.WriteLine("This number has no 3rd digit"); 
+}
+
+
