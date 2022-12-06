90c63e8d9d24a420207fd76287ac2c3c1da005c0
Vyacheslav <slava-russia@mail.ru>
2022-12-06 17:59:03 +0700

    Task 23 completed

---
+++ Homework_03/Task_23/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Error! Please enter number.");
+        }
+    }
+
+    return result;
+}
+
+void PrintCubeTable(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write($"{i * i * i} ");
+    }
+}
+
+int number = GetNumber("Enter number");
+
+PrintCubeTable(number);
