43a607bb9904f8d0a78245d779ca5b86f21f3661
Vyacheslav <slava-russia@mail.ru>
2022-12-10 16:19:20 +0700

    Task 27 completed

---
+++ Homework_04/Task_27/Program.cs
@@ -0,0 +1,39 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Error! Please enter number.");
+        }
+    }
+
+    return result;
+}
+
+int SumOfNumDigits(int num)
+{
+    int sum = 0;
+    int lastDigit = num % 10; 
+    while (num > 0)
+    {
+        sum += lastDigit;
+        num /= 10;
+        lastDigit = num % 10;
+    }
+    return sum;
+}
+
+int number = GetNumber("Enter some number");
+int result = SumOfNumDigits(number);
+Console.WriteLine(result);
