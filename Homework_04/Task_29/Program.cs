1f3f2041f4ece1cbb31b026c507ea6d363014d98
Vyacheslav <slava-russia@mail.ru>
2022-12-10 16:51:59 +0700

    Task 29 completed

---
+++ Homework_04/Task_29/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Error! Please enter number.");
+        }
+    }
+
+    return result;
+}
+
+int[] GetArray(int arrayLength)
+{
+    int[] arr = new int[arrayLength];
+    Random rnd = new Random();
+    for (int i = 0; i < arrayLength; i++)
+    {
+        arr[i] = rnd.Next(1, 10);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int arrLen = GetNumber("Enter array length");
+int[] array = GetArray(arrLen);
+PrintArray(array);
