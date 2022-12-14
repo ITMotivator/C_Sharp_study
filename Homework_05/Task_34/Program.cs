95631924eba71ed77f9c4a98bebf85a964a3e66a
Vyacheslav <slava-russia@mail.ru>
2022-12-14 18:34:27 +0700

    Task 34 completed

---
+++ Homework_05/Task_34/Program.cs
@@ -0,0 +1,65 @@
+﻿/* Задайте массив заполненный случайными положительными трёхзначными числами. 
+/* Напишите программу, которая покажет количество чётных чисел в массиве.
+
+/*Метод получения числа с консоли*/
+
+int GetSizeMassive(string message)
+{
+    int result;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Введено неверное число, повторить ввод");
+        }
+    }
+    return result;
+}
+// Метод инициализации массива
+
+int[] InitArray(int razmer)
+{
+    int[] arr = new int[razmer];
+    Random rnd = new Random();
+    for (int i = 0; i < razmer; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+// Метод подсчета четных чисел в массиве
+
+int GetNumOfEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) 
+        {
+count++;
+        }
+    }
+    return count;
+}
+
+void PrintArraySign(int[] massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        Console.Write($"{massiv[i]} ");
+
+    }
+    Console.WriteLine();
+}
+
+int number = GetSizeMassive("Введите размерность массива: ");
+int[] newArr = InitArray(number);
+int evenCount = GetNumOfEvenNumbers(newArr);
+PrintArraySign(newArr);
+Console.WriteLine($"В массиве четных чисел: {evenCount}");
