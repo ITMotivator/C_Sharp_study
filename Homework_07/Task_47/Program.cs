71edb26c7f9197b538ee57b43c4379b8cd73f4db
Vyacheslav <slava-russia@mail.ru>
2022-12-21 17:49:30 +0700

    Task 47 completed

---
+++ Homework_07/Task_47/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Not a number");
+        }
+    }
+    return result;
+}
+
+double[,] InitMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.NextDouble() * 100, 2);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int m = GetNumber("Enter number of rows");
+int n = GetNumber("Enter number of columns");
+
+double[,] matrix = InitMatrix(m, n);
+PrintArray(matrix);
