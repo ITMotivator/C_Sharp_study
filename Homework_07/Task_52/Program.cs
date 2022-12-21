e1ae282d38c30f6ec72b50885c2609e56b3a3e7a
Vyacheslav <slava-russia@mail.ru>
2022-12-21 18:57:49 +0700

    Task 52 completed

---
+++ Homework_07/Task_52/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+// элементов в каждом столбце.
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Not a number");
+        }
+    }
+    return result;
+}
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+//Метод нахождения среднего арифметического по столбцу
+
+void GetAvgArithInColumn(int[,] matrix) 
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        double res = Math.Round((double)sum / matrix.GetLength(0), 2); 
+        Console.Write($"{res} ");
+    }
+}
+
+
+int m = GetNumber("Enter number of rows");
+int n = GetNumber("Enter number of columns");
+
+int[,] matrix = InitMatrix(m, n);
+PrintArray(matrix);
+GetAvgArithInColumn(matrix);
