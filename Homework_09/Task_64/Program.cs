a307e74d309bd356c51d99c2f0b377eda403cb6e
Vyacheslav <slava-russia@mail.ru>
2022-12-28 18:40:09 +0700

    Task 64 completed

---
+++ Homework_09/Task_64/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result;
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Not a number");
+        }
+    }
+    return result;
+}
+
+void GetRow(int number)
+{
+    if (number == 1)
+    {
+        Console.Write($"{number} ");
+    }
+    else
+    {
+        Console.Write($"{number} ");
+        GetRow(number - 1);
+    }
+}
+
+int num = GetNumber("Enter number");
+GetRow(num);
